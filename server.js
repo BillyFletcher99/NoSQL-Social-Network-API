bf0b9ee2cf76cc23692f9bfc005570b9eb15134b
Billy Fletcher <Billyfletcher99@gmail.com>
2022-09-29 20:56:29 -0400

    server.js push

--- server.js
+++ server.js
@@ -2,3 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001; 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(require('./routes'));
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/NoSQL-Social-Network-API', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => 
+    console.log(`API server running on port ${PORT}!`)
+  );
